d7d4028ad8ecae40376125357c5f913e992d59e1
datitisev <dsevilla192@icloud.com>
2019-06-15 20:18:08 +0000

    Apply fixes from StyleCI

--- src/Extend/ExtensionSettings.php
+++ src/Extend/ExtensionSettings.php
@@ -1,13 +1,22 @@
 <?php
 
+/*
+ * This file is part of fof/extend.
+ *
+ * Copyright (c) 2019 FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
 namespace FoF\Extend\Extend;
 
 use Flarum\Extend\ExtenderInterface;
-use Flarum\Frontend\Document;
-use Illuminate\Contracts\Container\Container;
 use Flarum\Extension\Extension;
+use Flarum\Frontend\Document;
 use Flarum\Frontend\Frontend;
 use Flarum\Settings\SettingsRepositoryInterface;
+use Illuminate\Contracts\Container\Container;
 
 class ExtensionSettings implements ExtenderInterface
 {
@@ -29,13 +38,15 @@ class ExtensionSettings implements ExtenderInterface
     /**
      * @param string $frontend
      */
-    public function __construct() {
+    public function __construct()
+    {
         $this->settings = app(SettingsRepositoryInterface::class);
     }
 
-    public function extend(Container $container, Extension $extension = null) {
+    public function extend(Container $container, Extension $extension = null)
+    {
         $container->resolving(
-            "flarum.frontend.forum",
+            'flarum.frontend.forum',
             function (Frontend $frontend, Container $container) {
                 $frontend->content(function (Document $document) {
                     foreach ($this->keys as $key) {
@@ -47,42 +58,48 @@ class ExtensionSettings implements ExtenderInterface
     }
 
     /**
-     * Set extension keys prefix
+     * Set extension keys prefix.
      *
      * @param string $prefix
+     *
      * @return self
      */
-    public function setPrefix($prefix) : self {
+    public function setPrefix($prefix) : self
+    {
         $this->prefix = $prefix;
 
         return $this;
     }
 
     /**
-     * Add setting key
+     * Add setting key.
      *
      * @param string $key
+     *
      * @return self
      */
-    public function addKey($key) : self {
-        $this-addKeys(array($key));
+    public function addKey($key) : self
+    {
+        $this - addKeys([$key]);
 
         return $this;
     }
 
     /**
-     * Add multiple setting keys
+     * Add multiple setting keys.
      *
      * @param array $keys
+     *
      * @return self
      */
-    public function addKeys(array $keys) : self {
+    public function addKeys(array $keys) : self
+    {
         if (isset($this->prefix)) {
             $keys = array_map(function ($key) {
                 return $this->prefix.$key;
             }, $keys);
         }
-        
+
         $this->keys = $keys;
 
         return $this;
