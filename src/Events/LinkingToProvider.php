6e7f7a536677de16890a54bf0eacd454f2b4ac7c
Ian Morland <ian@morland.me>
2023-09-01 08:32:57 +0100

    chore: refactor oauth controller for readability, add linking event

---
+++ src/Events/LinkingToProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace FoF\Extend\Events;
+
+use Flarum\User\User;
+
+class LinkingToProvider
+{
+    /**
+     * The OAuth provider name. This is used in the `login_providers` table as the `provider` column.
+     *
+     * @var string
+     */
+    public $providerName;
+
+    /**
+     * The providers unique identifier as given by the provider. This is used in the `login_providers` table as the `identifier` column.
+     *
+     * @var string
+     */
+    public $identifier;
+
+    /**
+     * The user object that is attempting to link their account.
+     *
+     * @var User
+     */
+    public $actor;
+
+    public function __construct(string $providerName, string $identifier, User $actor)
+    {
+        $this->providerName = $providerName;
+        $this->identifier = $identifier;
+        $this->actor = $actor;
+    }
+}
